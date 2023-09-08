011176ee687067bfd981a5f690812bd4cd79efbe
AndreyWBS <andrey.wesley.sa@gmail.com>
2023-09-08 08:38:13 -0300

    começando o projeo e baixando as libs

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+
+options = webdriver.ChromeOptions()
+#options.add_argument("--headless=new")
+caminho_nav = r"user-data-dir=C:\Users\gabri\AppData\Local\Temp\scoped_dir10596_811454473"
+options.add_argument(caminho_nav)
+driver = webdriver.Chrome(options=options)
+
+
+
+
+
+
+driver.get('https://web.whatsapp.com/')
+
+time.sleep(25)
+
+
+#C:\Users\gabri\AppData\Local\Temp\scoped_dir10596_811454473\Default
+#C:\Users\gabri\AppData\Local\Temp\scoped_dir2968_973316188\Default
+
+"""
+chrome_options = Options()
+chrome_options.add_argument("user-data-dir=C:/Users/e5512459/AppData/Local/Google/Chrome/User Data")
+self.brs = webdriver.Chrome(executable_path="C:/Users/e5512459/PycharmProjects/wpp/chromedriver.exe", chrome_options=chrome_options)
+"""
